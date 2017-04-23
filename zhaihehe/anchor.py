616a47f9d78892f852215750bd3c45b4375e1136
ZHANG WenYao <wenyao.zhang@unifr.ch>
2017-04-23 18:36:40 +0200

    fix zhaihehe anchor

--- zhaihehe/anchor.py
+++ zhaihehe/anchor.py
@@ -25,8 +25,14 @@ def crawl(date=None, list='0', city='0', sex='0', fans='0', outfolder=None,
         os.makedirs(fld)
 
     url = '{}/{}'.format(config['api']['anchor'], url)
-    range1 = int(range1)
-    range2 = int(range2)
+    if not range1:
+        range1 = 0
+    else:
+        range1 = int(range1)
+    if not range2:
+        range2 = 0
+    else:
+        range2 = int(range2)
     if range1 <= 1 or range2 <= 0:
         print('crawl url:', url)
         html = requests.get(url)
@@ -42,8 +48,7 @@ def crawl(date=None, list='0', city='0', sex='0', fans='0', outfolder=None,
             fn = os.path.join(fld, '1.txt')
             print('save as:', fn)
             with open(fn, 'w') as f:
-                head = soup.find("div", {"id": "table_title"})
-                f.write(anchor_row(head))
+                f.write(anchor_head(soup))
                 table = soup.find(
                     "div", {"class": "table-body company-list-body"})
                 for row in table.findAll("div", {"class": "table-row ng-scope"}):
@@ -52,23 +57,34 @@ def crawl(date=None, list='0', city='0', sex='0', fans='0', outfolder=None,
     for i in range(range1, range2):
         url2 = '{}/&p={}'.format(url, i)
         fn = os.path.join(fld, str(i) + '.txt')
-        anchor_page(url2, fn)
+        try:
+            anchor_page(url2, fn)
+        except:
+            print('crawl', url2, 'error')
     return
 
 
-def anchor_row(t):
-    name = t.find('div', {'class': 'name'})
-    if name is None:
-        name = ''
-        t = str(t)
-    else:
-        name = name.get_text().strip() + '\t'
-        t = re.sub('[\s\S]*<div class="table-col founder">', '', str(t))
+def anchor_head(t):
+    t = t.find("div", {"id": "table_title"})
     t = re.sub('<!--', '', str(t))
     t = re.sub('-->', '', t)
     t = re.sub('\s*<[^>]*>\s*', '\t', t)
     t = re.sub('\t+', '\t', t)
-    return name + t.strip() + '\n'
+    return t.strip() + '\n'
+
+
+def anchor_row(t):
+    name = t.find('div', {'class': 'name'})
+    name = name.get_text() if name else ''
+    d = t.findAll('div', {'class': 'table-col founder'})
+    id = d[0].get_text()
+    # platform=d[1].get_text()
+    t = re.sub('[\s\S]*<div class="table-col founder">', '', str(t))
+    t = re.sub('<!--', '', str(t))
+    t = re.sub('-->', '', t)
+    t = re.sub('\s*<[^>]*>\s*', '\t', t)
+    t = re.sub('\t+', '\t', t)
+    return name.strip() + '\t' + id.strip() + '\t' + t.strip() + '\n'
 
 
 def anchor_page(url, fn):
@@ -77,8 +93,7 @@ def anchor_page(url, fn):
     soup = BeautifulSoup(html.text, 'lxml')
     print('save as:', fn)
     with open(fn, 'w') as f:
-        head = soup.find("div", {"id": "table_title"})
-        f.write(anchor_row(head))
+        f.write(anchor_head(soup))
         table = soup.find("div", {"class": "table-body company-list-body"})
         for row in table.findAll("div", {"class": "table-row ng-scope"}):
             f.write(anchor_row(row))
